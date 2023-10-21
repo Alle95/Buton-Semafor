8672b6de4d24086db811d0fd66b74d3b00a27fdf
Alle95 <147329984+Alle95@users.noreply.github.com>
2023-10-21 17:51:33 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const btn = document.getElementById('myButton');
+const colors = ['yellow', 'green', 'red'];
+let index = 0;
+var i = 0;
+
+btn.addEventListener('click', function onClick() {
+        btn.style.backgroundColor = colors[index];
+        btn.style.color = 'black';
+        index = index >= colors.length -1 ? 0 : index + 1;
+})
+
+function changeColor() {
+    btn.style.backgroundColor = colors[index];
+    btn.style.color = 'black';
+    index = index >= colors.length -1 ? 0 : index + 1;
+}
+setInterval(changeColor, 10000);
